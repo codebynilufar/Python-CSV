985ebb3fbdcb3ee279c351440b99bb81c2403ca1
codebynilufar <nilufarhamroyeva2006@gmail.com>
2025-07-03 15:13:16 +0500

    done

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,19 @@
+import csv
+with open("students.csv",  newline="") as f:
+    students = list(csv.DictReader(f))
+    students.sort(key=lambda x: int(x["score"]), reverse=True)
 
+
+with open("rating.csv", "w", newline="") as f2:
+    fieldnames = ["name", "score"]
+
+    writer = csv.DictWriter(f2, fieldnames=fieldnames)
+    
+    writer.writeheader()
+    writer.writerows(students)
+
+
+
+
+
+       
